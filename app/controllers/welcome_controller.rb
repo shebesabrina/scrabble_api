59402a82661a4ba11190873905b5b1e1779cc98d
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-25 10:36:40 -0600

    Got reponse.body and working on steps to get work and sentances

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    conn = Faraday.new('https://od-api.oxforddictionaries.com/api/v1/')
-    conn.get('entries/en/mindfulness/sentences')
-    response = Faraday.get('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/mindfulness/sentences')
+    conn = Faraday.new('https://od-api.oxforddictionaries.com:443/api/v1/')
+    response = conn.get('entries/en/mindfulness/sentences') do |req|
+      # req.url 'https://od-api.oxforddictionaries.com/api/v1/'
+      req.headers['app_id'] = ENV['app_id']
+      req.headers['app_key'] = ENV['app_key']
+    end
+     response_data = JSON.parse(response.body)
+      response_data['results'].first["lexicalEntries"].first["sentences"].first["regions"] #this gives me N.America
+      response_data['results'].first["lexicalEntries"].first["sentences"].first["text"] #this gives me the first sentence
+      #I need a list of sentances. Iterate?
   end
 end
