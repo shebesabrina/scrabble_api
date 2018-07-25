b6fa5584b5f2003892b160a6a6bccc7acc9b5957
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-25 10:04:04 -0600

    Working on trying to get the headers because the body does not have the api auth

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    conn = Faraday.new('https://od-api.oxforddictionaries.com/api/v1/')
+    conn.get('entries/en/mindfulness/sentences')
     response = Faraday.get('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/mindfulness/sentences')
   end
 end
