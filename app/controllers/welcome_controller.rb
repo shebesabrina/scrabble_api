7e7d386c143e0bf3e473ca844acef63a822ae421
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-25 09:50:24 -0600

    Working on trying to get the body of the response

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    Faraday.new('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/mindfulness/sentences')
+    response = Faraday.get('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/mindfulness/sentences')
   end
 end
--- spec/features/feature_1_spec.rb
+++ spec/features/feature_1_spec.rb
@@ -10,6 +10,7 @@ describe 'As a registered guest' do
     # And I click "Submit"
     expect(page).to have_content('mindfulness')
     # Then I should see a message that says "Examples for using 'mindfulness'"
+    expect(page).to have_content("Examples for using 'mindfulness'")
     # And I should see a list of sentences with examples of how to use the word
     # And I should see only sentences for usage in North America
     # And I should not see sentences for any other regions
